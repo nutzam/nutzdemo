1f76f0541425deec604fa2903ecb409b89a6ef87
zozoh <zozohtnt@gmail.com>
2009-11-18 13:42:53 +0000

    git-svn-id: http://nutzdemo.googlecode.com/svn/trunk@32 686b29da-b713-11de-b017-a9a2c7051f87

--- demo/hello_mvc/src/nutz/demo/mvc/MainModule.java
+++ demo/hello_mvc/src/nutz/demo/mvc/MainModule.java
@@ -17,7 +17,7 @@ import org.nutz.mvc.init.JsonIocProvider;
  * <li>通过 '@IocBy' 注解声明整个应用，应采用何种方式进行反转注入。如果没有声明，整个应用将不支持 Ioc
  * <li>通过 '@Localization' 注解声明整个应用的本地地化字符串的目录
  * <li>通过 '@SetupBy' 注解声明应用启动的关闭时，应该进行的处理。（通过 Setup 接口）
- * <li>通过 '@Ok' 注解声明整个应用，默认的成功视图
+ * <li>通过 '@Ok' 注解声明整个应用默认的成功视图
  * <li>通过 '@Fail' 注解声明整个应用默认的失败视图
  * </ul>
  * 
