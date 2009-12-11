1a80626479c6e0e74f42731a822bea46ce9daefe
BirdWyatt <BirdWyatt@gmail.com>
2009-12-11 09:02:31 +0000

    add jquery ui support
    git-svn-id: http://nutzdemo.googlecode.com/svn/trunk@52 686b29da-b713-11de-b017-a9a2c7051f87

--- demo/pet_store/WebContent/WEB-INF/jsp/account/showallaccounts.jsp
+++ demo/pet_store/WebContent/WEB-INF/jsp/account/showallaccounts.jsp
@@ -14,30 +14,123 @@
 <base href="<%=basePath%>">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
+<link type="text/css" href="themes/base/ui.all.css" rel="stylesheet" />
 <script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/json2.js"></script>
+<script type="text/javascript" src="ui/ui.core.js"></script>
+<script type="text/javascript" src="ui/ui.draggable.js"></script>
+<script type="text/javascript" src="ui/ui.resizable.js"></script>
+<script type="text/javascript" src="ui/ui.dialog.js"></script>
+<script type="text/javascript" src="ui/effects.core.js"></script>
+<script type="text/javascript" src="ui/effects.highlight.js"></script>
+<script type="text/javascript" src="external/bgiframe/jquery.bgiframe.js"></script>
 <style type="text/css">
-.userid {
-	color: red;
-	event: expression(
-		onclick = function() {
-			alert(1);
-		}
-	);
-}
+	body { font-size: 62.5%; }
+	label font{color:red;}
+	label, input { display:block; }
+	input.text { margin-bottom:12px; width:95%; padding: .4em; }
+	fieldset { padding:0; border:0; margin-top:25px; }
+	h1 { font-size: 1.2em; margin: .6em 0; }
+	div#users-contain {  width: 350px; margin: 20px 0; }
+	div#users-contain table { margin: 1em 0; border-collapse: collapse; width: 100%; }
+	div#users-contain table td, div#users-contain table th { border: 1px solid #eee; padding: .6em 10px; text-align: left; }
+	.ui-button { outline: 0; margin:0; padding: .4em 1em .5em; text-decoration:none;  !important; cursor:pointer; position: relative; text-align: center; }
+	.ui-dialog .ui-state-highlight, .ui-dialog .ui-state-error { padding: .3em;  }
+	.errordialog{ text-align: center; width:100%;}
+	.errormessage{ color:red;}
 </style>
+<script type="text/javascript" src="js/account/showallaccounts.js"></script>
 <script type="text/javascript">
-	
+//$(document).ready()
+$(function() {
+	showAllAccounts.onLoadEvent();
+});
 </script>
 </head>
 <body>
-<table width="100%" border="1" cellpadding="0" cellspacing="0">
+<div id="error_message" title="Error" class="errordialog">
+	<p>
+		<span class="errormessage"></span>
+	</p>
+</div>
+
+<div id="create_dialog" title="Create new user">
+	<p id="validateTips">All form fields are required.</p>
+
+	<form>
+	<fieldset>
+		<label for="userid">User ID <font>*</font></label>
+		<input type="text" name="userid" id="userid" class="text ui-widget-content ui-corner-all" />
+		<label for="password">Password <font>*</font></label>
+		<input type="password" name="password" id="password" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="firstname">First Name <font>*</font></label>
+		<input type="text" name="firstname" id="firstname" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="lastname">Last Name <font>*</font></label>
+		<input type="text" name="lastname" id="lastname" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="phone">Phone <font>*</font></label>
+		<input type="text" name="phone" id="phone" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="email">Email <font>*</font></label>
+		<input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="address1">Address 1 <font>*</font></label>
+		<input type="text" name="address1" id="address1" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="address2">Address 2</label>
+		<input type="text" name="address2" id="address2" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="city">City <font>*</font></label>
+		<input type="text" name="city" id="city" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="state">State <font>*</font></label>
+		<input type="text" name="state" id="state" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="zip">Zip <font>*</font></label>
+		<input type="text" name="zip" id="zip" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="country">Country <font>*</font></label>
+		<input type="text" name="country" id="country" value="" class="text ui-widget-content ui-corner-all" />
+	</fieldset>
+	</form>
+</div>
+<div id="update_dialog" title="Update user">
+	<p id="validateTips_update">All form fields are required.</p>
+
+	<form>
+	<fieldset>
+		<label for="userid_create">User ID</label>
+		<input type="text" name="userid_create" id="userid_create" class="text ui-widget-content ui-corner-all" />
+		<label for="password_create">Password</label>
+		<input type="password" name="password_create" id="password_create" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="firstname_create">First Name</label>
+		<input type="text" name="firstname_create" id="firstname_create" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="lastname_create">Last Name</label>
+		<input type="text" name="lastname_create" id="lastname_create" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="phone_create">Phone</label>
+		<input type="text" name="phone_create" id="phone_create" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="email_create">Email</label>
+		<input type="text" name="email_create" id="email_create" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="address1_create">Address 1</label>
+		<input type="text" name="address1_create" id="address1_create" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="address2_create">Address 2</label>
+		<input type="text" name="address2_create" id="address2_create" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="city_create">City</label>
+		<input type="text" name="city_create" id="city_create" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="state_create">State</label>
+		<input type="text" name="state_create" id="state_create" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="zip_create">Zip</label>
+		<input type="text" name="zip_create" id="zip_create" value="" class="text ui-widget-content ui-corner-all" />
+		<label for="country_create">Country</label>
+		<input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all" />
+	</fieldset>
+	</form>
+</div>
+
+<div id="users-contain" class="ui-widget">
+
+		<h1>Existing Users:</h1>
+
+<table id="users" class="ui-widget ui-widget-content">
 	<thead>
-		<tr>
+		<tr class="ui-widget-header ">
 			<td>User ID</td>
-			<td>First Name</td>
-			<td>Last Name</td>
-			<td>Email Addreass</td>
-			<td>phone</td>
+			<th>First Name</th>
+			<th>Last Name</th>
+			<th>Email Addreass</th>
+			<th>Phone</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -52,5 +145,7 @@
 		</c:forEach>
 	</tbody>
 </table>
+</div>
+<button id="create-user" class="ui-button ui-state-default ui-corner-all">Create new user</button>
 </body>
 </html>
--- demo/pet_store/WebContent/js/account/showallaccounts.js
+++ demo/pet_store/WebContent/js/account/showallaccounts.js
@@ -1,5 +1,198 @@
 var showAllAccounts = {
+	onLoadEvent : function() {
+		$(document).ajaxError(function(event, request, settings){
+			$("#error_message span").text("Network Error");
+			$("#error_message").dialog("open");
+		});
+		userid = $("#userid"),
+		password = $("#password"),
+		firstname = $("#firstname"),
+		lastname = $("#lastname"),
+		phone = $("#phone"),
+		email = $("#email"),
+		address1 = $("#address1"),
+		address2 = $("#address2"),
+		city = $("#city"),
+		state = $("#state"),
+		zip = $("#zip"),
+		country = $("#country"),
+		allFields = $([]).add(userid).add(password).add(firstname).add(lastname).add(
+			phone).add(email).add(address1).add(address2).add(city).add(
+			state).add(zip).add(country);
+		var funcHolder = this;
+		$(".userid").bind("click", function() {
+			funcHolder.showAccountDetail($(this).text());
+		});
+		$("#error_message").dialog({
+			bgiframe: true,
+			modal: true,
+			autoOpen: false,
+			buttons: {
+				Ok: function() {
+					$(this).dialog('close');
+				}
+			}
+		});
+
+		$("#create_dialog").dialog({
+			bgiframe: true,
+			autoOpen: false,
+			height: 500,
+			modal: true,
+			buttons: {
+				'Create an account': function() {
+					funcHolder.createAccount($(this));
+				},
+				Cancel: function() {
+					$(this).dialog('close');
+				}
+			},
+			close: function() {
+				allFields.val('').removeClass('ui-state-error');
+			}
+		});
+		$("#update_dialog").dialog({
+			bgiframe: true,
+			autoOpen: false,
+			height: 300,
+			modal: true,
+			buttons: {
+				'Update an account': function() {
+					var bValid = true;
+					allFields.removeClass('ui-state-error');
+
+					bValid = bValid && funcHolder.checkLength(name,"username",3,16);
+					bValid = bValid && funcHolder.checkLength(email,"email",6,80);
+					bValid = bValid && funcHolder.checkLength(password,"password",5,16);
+
+					bValid = bValid && funcHolder.checkRegexp(name,/^[a-z]([0-9a-z_])+$/i,"Username may consist of a-z, 0-9, underscores, begin with a letter.");
+					// From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
+					bValid = bValid && funcHolder.checkRegexp(email,/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,"eg. ui@jquery.com");
+					bValid = bValid && funcHolder.checkRegexp(password,/^([0-9a-zA-Z])+$/,"Password field only allow : a-z 0-9");
+					
+					if (bValid) {
+						$('#users tbody').append('<tr>' +
+							'<td>' + name.val() + '</td>' + 
+							'<td>' + email.val() + '</td>' + 
+							'<td>' + password.val() + '</td>' +
+							'</tr>'); 
+						$(this).dialog('close');
+					}
+				},
+				Cancel: function() {
+					$(this).dialog('close');
+				}
+			},
+			close: function() {
+				allFields.val('').removeClass('ui-state-error');
+			}
+		});
+		$('#create-user').click(function() {
+			$('#create_dialog').dialog('open');
+		})
+		.hover(
+			function(){ 
+				$(this).addClass("ui-state-hover"); 
+			},
+			function(){ 
+				$(this).removeClass("ui-state-hover"); 
+			}
+		).mousedown(function(){
+			$(this).addClass("ui-state-active"); 
+		})
+		.mouseup(function(){
+				$(this).removeClass("ui-state-active");
+		});
+	},
 	showAccountDetail : function(userid) {
-		alert(userid);
+		$('#update_dialog').dialog('open');
+	},
+	createAccount : function(dialog) {
+		var bValid = true;
+		allFields.removeClass('ui-state-error');
+
+		bValid = bValid && this.checkLength(userid,"username",3,16);
+		bValid = bValid && this.checkLength(password,"password",5,16);
+		bValid = bValid && this.checkLength(firstname,"firstname",3,16);
+		bValid = bValid && this.checkLength(lastname,"lastname",3,16);
+		bValid = bValid && this.checkLength(email,"email",6,80);
+		bValid = bValid && this.checkLength(address1,"address1",3,16);
+		bValid = bValid && this.checkLength(city,"city",3,16);
+		bValid = bValid && this.checkLength(state,"state",3,16);
+		bValid = bValid && this.checkLength(zip,"zip",3,16);
+		bValid = bValid && this.checkLength(country,"country",3,16);
+		
+
+		bValid = bValid && this.checkRegexp(userid,/^[a-z]([0-9a-z_])+$/i,"Username may consist of a-z, 0-9, underscores, begin with a letter.");
+		bValid = bValid && this.checkRegexp(firstname,/^[a-z]([0-9a-z_])+$/i,"Firstname may consist of a-z, 0-9, underscores, begin with a letter.");
+		bValid = bValid && this.checkRegexp(lastname,/^[a-z]([0-9a-z_])+$/i,"Lastname may consist of a-z, 0-9, underscores, begin with a letter.");
+		// From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
+		bValid = bValid && this.checkRegexp(email,/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,"eg. ui@jquery.com");
+		bValid = bValid && this.checkRegexp(password,/^([0-9a-zA-Z])+$/,"Password field only allow : a-z 0-9");
+		
+		if (bValid) {
+			var account={};
+			account.userid=userid.val();
+			account.email=email.val();
+			account.firstName=firstname.val();
+			account.lastName=lastname.val();
+			account.address1=address1.val();
+			account.address2=address2.val();
+			account.city=city.val();
+			account.state=state.val();
+			account.zip=zip.val();
+			account.country=country.val();
+			account.phone=phone.val();
+			$.ajax({
+		        url:"mvc/account/createAccount.do",
+		        type:"GET",
+		        data:"account="+encodeURIComponent(JSON.stringify(account)),
+		        dataType:"json",
+		        success :function (result){
+					if(result.detailMessage){
+						alert(result.detailMessage);
+					}else{
+			            
+			            $('#users tbody').append('<tr>' +
+			    				'<td>' + result.userid + '</td>' + 
+			    				'<td>' + result.firstName + '</td>' + 
+			    				'<td>' + result.lastName + '</td>' +
+			    				'<td>' + result.email + '</td>' +
+			    				'<td>' + result.phone + '</td>' +
+			    				'</tr>');
+			            dialog.dialog('close');
+					}
+		        }
+			});
+			
+		}
+	},
+	updateTips : function(t) {
+		var tips = $("#validateTips");
+		tips.text(t).effect("highlight", {}, 1500);
+	},
+
+	checkLength : function(o, n, min, max) {
+
+		if (o.val().length > max || o.val().length < min) {
+			o.addClass('ui-state-error');
+			this.updateTips("Length of " + n + " must be between " + min + " and "
+					+ max + ".");
+			return false;
+		} else {
+			return true;
+		}
+
+	},
+	checkRegexp : function(o, regexp, n) {
+
+		if (!(regexp.test(o.val()))) {
+			o.addClass('ui-state-error');
+			this.updateTips(n);
+			return false;
+		} else {
+			return true;
+		}
+
 	}
 };
