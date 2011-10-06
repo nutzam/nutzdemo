643c3df51b74831546e9527c6f4c4603b46ed641
wendal <wendal1985@gmail.com>
2011-10-06 21:49:30 +0800

    fix simpleuploads fail to use

--- demo/simpleUploads/src/org/nutz/website/pan/service/FileInfoService.java
+++ demo/simpleUploads/src/org/nutz/website/pan/service/FileInfoService.java
@@ -4,7 +4,7 @@ import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.service.IdEntityService;
 import org.nutz.website.pan.bean.FileInfo;
 
-@IocBean(field={"dao"})
+@IocBean(fields={"dao"})
 public class FileInfoService extends IdEntityService<FileInfo> {
 
 }
